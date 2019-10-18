586bd070e9af5e5eca5c8033ed0c2ea6be3a5ccc
Tdspenc97 <28689801+Tdspenc97@users.noreply.github.com>
2019-10-17 20:03:02 -0500

    fixed weather update

--- src/weather.py
+++ src/weather.py
@@ -7,16 +7,12 @@ class Weather(Widget):
 
 	def init_components(self):
 		self.request = Requestweather()
-		self.request.requestdata()
-		self.request.requesticon(self.request.icon)
-		self.label1 = Label(self, bg="black", foreground="white", font=("mono", 32, "bold"),text=self.request.temp+'°')
+		self.label1 = Label(self, bg="black", foreground="white", font=("mono", 32, "bold"))
 		self.label1.pack(anchor=N)
-		self.label2 = Label(self,bg="black", foreground="white", font=("mono", 24, "bold"),text=self.request.description)
+		self.label2 = Label(self,bg="black", foreground="white", font=("mono", 24, "bold"))
 		self.label2.pack(anchor=S)
-		self.weatherimage = PhotoImage(data=self.request.iconimage)
-		self.iconcheck = self.request.icon
-		print(self.request.iconimage)
-		self.create_image(0, 0, anchor="nw", image=self.weatherimage)
+		self.iconcheck = None
+		
 	def update(self):
 		self.request.requestdata()
 		print(self.request.temp)
@@ -25,8 +21,10 @@ class Weather(Widget):
 			self.label2.config(text=self.request.description)
 		if self.iconcheck != self.request.icon:
 			self.request.requesticon(self.request.icon)
-			self.iconcheck = self.request.icon
-		self.after(180000,self.update) 
+			self.iconcheck = self.request.icon 
+			self.weatherimage = PhotoImage(data=self.request.iconimage)
+			self.create_image(0, 0, anchor="nw", image=self.weatherimage)
+		self.after(180000,self.update)
 	
 #request = Weather()
 #print(request.temp)
